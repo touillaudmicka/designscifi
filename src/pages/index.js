00a6f9164f013d2e5bf50786fc1ade8adb5cc7c9
Micka Touillaud <micka.touillaud@me.com>
2018-09-21 23:20:59 -0700

    first setup

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <div>
+    <Layout>
+      <h1>Explore possible future 🔮</h1>
+      <p>Explore the most iconic science fiction designs, download original designs and create what’s next..</p>
+      <Link to="/page-2/">GET EARLY ACCESS</Link>
+    </Layout>
+  </div>
+)
+
+export default IndexPage
