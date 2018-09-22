76f66cf9d183206b0750121efd9099ba508eb848
Micka Touillaud <micka.touillaud@me.com>
2018-09-22 00:17:58 -0700

    basic css and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,16 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
-  <div>
-    <Layout>
-      <h1>Explore possible future 🔮</h1>
-      <p>Explore the most iconic science fiction designs, download original designs and create what’s next..</p>
-      <Link to="/page-2/">GET EARLY ACCESS</Link>
-    </Layout>
+  <div className="Hero">
+    
+    <div className="HeroGroup">
+     <h1>Explore possible future 
+       🔮</h1>
+     <p>Explore the most iconic science fiction designs, download original designs and create what’s next..</p>
+     <Link to="/page-2/">GET EARLY ACCESS</Link>
+
+    </div>
+
   </div>
 )
 
